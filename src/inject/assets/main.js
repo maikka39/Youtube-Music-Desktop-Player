0d654e73ef627a1dfa39b1e38aeed646d5a42a8f
Maik de Kruif <maik@maik.dev>
2019-10-07 21:14:05 +0200

    Add navigation buttons

--- src/inject/assets/main.js
+++ src/inject/assets/main.js
@@ -1,1 +1,13 @@
-console.log('Test')
+const leftArrow = document.createElement('span')
+leftArrow.innerText = '➜'
+leftArrow.setAttribute('onclick', 'window.history.back()')
+leftArrow.setAttribute('class', 'navigate_arrow reverted_arrow')
+
+const rightArrow = document.createElement('span')
+rightArrow.innerText = '➜'
+rightArrow.setAttribute('onclick', 'window.history.forward()')
+rightArrow.setAttribute('class', 'navigate_arrow')
+
+const centerbar = document.querySelector('.center-content.ytmusic-nav-bar')
+centerbar.insertBefore(leftArrow, centerbar.children[0])
+centerbar.appendChild(rightArrow)
